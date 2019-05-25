a77bf918478bb0901e1744aa660a21290ab3bea7
BielzerA23 <gabriel.ribeiro20110@hotmail.com>
2019-05-25 17:37:45 -0300

    Arquivo de log

---
+++ FrontSwing/jOshi2/jOshi2/src/main/java/MainLog.java
@@ -0,0 +1,9 @@
+public class MainLog {
+    public MainLog() {
+       new arquivoLog("projeto iniciado");
+    }
+    public static void main(String[] args) {
+        new arquivoLog("iniciando projeto");
+        new MainLog();
+    }
+}
