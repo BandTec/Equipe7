61933201018079ed286fdb2c5683f5a2771d1580
b4iano <lucas-lima2000@Hotmail.com>
2019-05-31 20:19:21 -0300

    Coletor Java

--- FrontSwing/jOshi2/jOshi2/src/main/java/MainLog.java
+++
@@ -1,9 +0,0 @@
-public class MainLog {
-    public MainLog() {
-       new arquivoLog("projeto iniciado");
-    }
-    public static void main(String[] args) {
-        new arquivoLog("iniciando projeto");
-        new MainLog();
-    }
-}
